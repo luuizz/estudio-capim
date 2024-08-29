61e1d37af59c0be279855f1f478bd7a26fe3496b
luuizz <luu.izz@hotmail.com>
2024-08-28 22:09:26 -0300

    feat: links menu

---
+++ src/utils/data.ts
@@ -0,0 +1,23 @@
+type MenuSocialItem = {
+  url: string;
+  label: string;
+};
+
+export const menuSocial: MenuSocialItem[] = [
+  {
+    url: "#",
+    label: "Behance",
+  },
+  {
+    url: "#",
+    label: "Instagram",
+  },
+  {
+    url: "#",
+    label: "Linkedin",
+  },
+  {
+    url: "#",
+    label: "Linktree",
+  },
+];
