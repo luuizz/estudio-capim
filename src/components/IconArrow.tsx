c10dccd2e6174b998ae81d42abf42ea88d8419f4
luuizz <luu.izz@hotmail.com>
2024-08-28 22:10:05 -0300

    feat: adicionado icon

---
+++ src/components/IconArrow.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export function IconArrow() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="12"
+      height="12"
+      fill="none"
+      viewBox="0 0 12 12"
+    >
+      <path
+        fill="#fff"
+        d="M2.463 11.64L.766 9.944 7.932 2.77H2.548L2.562.455h9.383v9.389h-2.33l.014-5.377-7.166 7.174z"
+      ></path>
+    </svg>
+  );
+}
