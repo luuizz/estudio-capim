3646498c14421719b522fcd18ad89294947e46f9
luuizz <luu.izz@hotmail.com>
2024-08-28 22:07:53 -0300

    feat: configurado reset

--- src/app/app.css.ts
+++ src/app/app.css.ts
@@ -6,5 +6,25 @@ const globalStyles = [
     padding: 0,
     boxSizing: "border-box",
     WebkitFontSmoothing: "antialiased",
+    color: "black",
+  }),
+
+  globalStyle("body", {
+    backgroundColor: "white",
+    fontSize: "62.5%",
+  }),
+
+  globalStyle("ul", {
+    listStyle: "none",
+  }),
+
+  globalStyle("a", {
+    textDecoration: "none",
+    display: "block",
+  }),
+
+  globalStyle("img", {
+    maxWidth: "100%",
+    display: "block",
   }),
 ];
